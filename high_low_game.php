53a4ae02803715125c8e1c4a313ab7b43b3f391d
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-12 15:11:50 -0500

    Added notes

--- high_low_game.php
+++ high_low_game.php
@@ -1,12 +1,14 @@
 <?php
 
-// Game picks number from 1 to 100.
+// User provides low and high numbers for computer.
 define('LOWEND', $argv[1]);
 define('HIEND', $argv[2]);
+// Error if two numbers are not provided.
 if ($argc < 3) {
 	echo "Please enter two numbers to play.\n";
 	exit(1);
 }
+// Computer generates ranomd number between user defined bounds.
 $number = mt_rand(LOWEND, HIEND);
 // User is prompted to guess a number.
 fwrite(STDOUT, "Guess a number from " . LOWEND . " and " . HIEND . " in ten guesses to win.\n");
@@ -17,16 +19,16 @@ $guesses = 1;
 while ($guess != $number && $guesses <= 10){
 	if ($guess > $number){
 		fwrite(STDOUT, "Nice try, but go lower! \n");
-		$guess = (int)fgets(STDIN);
-		$guesses++;
-	}
+		}
 	else {
 	fwrite(STDOUT, "Nice try, but go higher! \n");
-		$guess = (int)fgets(STDIN);
-		$guesses++;
+		
 	}
+	$guess = (int)fgets(STDIN);
+	$guesses++;
 }
-// If user guesses number, game ends with winning message.
+// If user guesses number, game ends with winning message. If user fails to guess
+// number in a certain number of guesses, user loses.
 if ($guesses <= 10){
 	fwrite(STDOUT, "YOU WIN!!11!1!\n");
 	fwrite(STDOUT, "It took you $guesses guesses.\n");
