d8180b30f09dad88abc12f11c8db97d8e832b67b
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-12 11:08:06 -0500

    Added mt_rand() function, cleaned up notes

--- high_low_game.php
+++ high_low_game.php
@@ -1,12 +1,12 @@
 <?php
 
 // Game picks number from 1 to 100.
-$number = rand(1,100);
+$number = mt_rand(1,100);
 // User is prompted to guess a number.
 fwrite(STDOUT, "Guess a number from 1 to 100! \n");
 $guess = (int) fgets(STDIN);
 
-// If user guesses number, game ends with winning message
+// If guess is wrong, feedback given and user asked for another guess.
 while ($guess != $number){
 	if ($guess > $number){
 		fwrite(STDOUT, "Nice try, but go lower! \n");
@@ -17,7 +17,7 @@ while ($guess != $number){
 		$guess = (int)fgets(STDIN);
 	}
 }
-
+// If user guesses number, game ends with winning message.
 fwrite(STDOUT, "YOU WIN!!!11!\n");
 exit(0);
 
