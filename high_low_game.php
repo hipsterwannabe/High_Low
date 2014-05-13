4190bf38cf726d6e56bf47dec0e1ddd854699adb
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-12 21:10:26 -0500

    fixed guesses counter bug

--- high_low_game.php
+++ high_low_game.php
@@ -4,11 +4,11 @@
 define('LOWEND', $argv[1]);
 define('HIEND', $argv[2]);
 // Error if two numbers are not provided.
-if ($argc < 3) {
-	echo "Please enter two numbers to play.\n";
+if ($argc != 3 || $argv[1]==$argv[2]) {
+	echo "Please enter two unique numbers to play.\n";
 	exit(1);
 }
-// Computer generates ranomd number between user defined bounds.
+// Computer generates random number between user defined bounds.
 $number = mt_rand(LOWEND, HIEND);
 // User is prompted to guess a number.
 fwrite(STDOUT, "Guess a number from " . LOWEND . " and " . HIEND . " in ten guesses to win.\n");
@@ -16,7 +16,7 @@ $guess = (int) fgets(STDIN);
 $guesses = 1;
 
 // If guess is wrong, feedback given and user asked for another guess.
-while ($guess != $number && $guesses <= 10){
+while ($guess != $number && $guesses < 10){
 	if ($guess > $number){
 		fwrite(STDOUT, "Nice try, but go lower! \n");
 		}
@@ -29,7 +29,7 @@ while ($guess != $number && $guesses <= 10){
 }
 // If user guesses number, game ends with winning message. If user fails to guess
 // number in a certain number of guesses, user loses.
-if ($guesses <= 10){
+if ($guess == $number){
 	fwrite(STDOUT, "YOU WIN!!11!1!\n");
 	fwrite(STDOUT, "It took you $guesses guesses.\n");
 } else {
